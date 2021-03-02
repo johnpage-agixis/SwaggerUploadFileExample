344d09e214778f52b6bbdb21fa194e2201074851
John Page <john.page@agiteam.fr>
2021-03-02 19:28:56 +0100

    Add missing file

---
+++ SwaggerFileUpload/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerFileUpload.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<FileController> _logger;
+
+        public FileController(ILogger<FileController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// File upload
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("test/")]
+        public ActionResult Upload(IFormFile file)
+        {
+            return Ok($"{file.FileName} is {file.Length} bytes long");
+        }
+    }
+}
